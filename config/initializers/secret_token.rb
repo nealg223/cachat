deac8e44f3a4f6f4168122f553ed7505b0a7f122
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2012-03-19 21:28:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cachat::Application.config.secret_token = 'dbed102c78b11b656f37410eef0f2cb2647d21c50c98044eae7752e781eb909dddd3bc76ff3ecf5a467d406306dc90b036d9e6717433862db62c9eb3c7af1f40'
