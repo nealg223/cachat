106347801a0d700de0d925b488ddb8a50bc0f5cd
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2012-03-23 22:06:06 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Cachat::Application.routes.draw do
   
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   
   root :to => "static_pages#home"
   
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  describe "Home page" do
+  
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+  end
+  
+  describe "Help page" do
+    
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+  end
+  
+  describe "About page" do
+    
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+  end
+end
