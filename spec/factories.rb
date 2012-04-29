f30e014cc60f521395b2ac6815375b7cd97bd4a2
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2012-04-28 20:30:25 -0500

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name      "Neal Sales-Griffin"
-    email     "neal@codeacademy.org"
-    password  "noelle"
-    password_confirmation "noelle"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person#{n}@example.com" }
+    password  "foobar"
+    password_confirmation "foobar"
+    
+    factory :admin do
+      admin true
+    end
   end
 end
